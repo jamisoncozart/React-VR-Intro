4c42d8bb10da85cd4232e24e376f5b82625c6667
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-10 13:40:36 -0700

    Add custom StyleSheet object to apply styling to View elements

--- index.vr.js
+++ index.vr.js
@@ -1,29 +1,46 @@
 import React, { Component } from 'react';
 
-import { AppRegistry, asset, Pano, View, Text } from 'react-vr';
+import { AppRegistry, asset, Pano, View, Text, StyleSheet } from 'react-vr';
 
-export default class Basics extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      showSign: true
-    };
-
-    setInterval(() => this.setState({showSign: !this.state.showSign}), 1000);
-  }
-
+class Row extends Component {
   render() {
-    let message = this.state.showSign === true ? 'Welcome to VR Land!' : " ";
-
-    return (
-      <View>
-        <Pano source={asset('starry-sky.jpg')}></Pano>
-        <Text style={{fontSize: 0.2, transform: [{translate: [-1, 0, -2]}]}}>{message}</Text>
+    return(
+      <View style={[{backgroundColor: this.props.color}, styles.row]}>
+        <Text style={styles.text}>{this.props.color}</Text>
       </View>
     )
   }
+}
 
+export default class Basics extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Pano source={asset('starry-sky.jpg')}></Pano>
+        <Row color='red' />
+        <Row color='blue' />
+        <Row color='green' />
+      </View>
+    )
+  }
 };
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 0.2,
+    textAlign: 'center'
+  },
+  row: {
+    width: 0.5,
+    height: 0.3,
+    margin: 0.1
+  },
+  container: {
+    flex: 1,
+    width: 2,
+    flexDirection: 'row',
+    transform: [{translate: [-1, 0, -3]}]
+  }
+})
 
 AppRegistry.registerComponent('Basics', () => Basics);
