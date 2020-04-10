0b8499e1b67cbc1677d6e90cd2d49f331cfa304d
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-10 14:08:46 -0700

    Add interactivity to change font size onEnter and onExit

--- index.vr.js
+++ index.vr.js
@@ -1,28 +1,28 @@
 import React, { Component } from 'react';
 
-import { AppRegistry, asset, Pano, View, Text, StyleSheet, Box } from 'react-vr';
+import { AppRegistry, asset, Pano, View, Text } from 'react-vr';
 
 export default class Basics extends Component {
+  constructor() {
+    super();
+    this.state={
+      fontSize: 0.1
+    }
+  }
+
   render() {
     return (
       <View>
         <Pano source={asset('starry-sky.jpg')}></Pano>
-        <Box 
-          dimWidth={0.5}
-          dimHeight={0.5}
-          dimDepth={0.5}
-          wireframe={true}
+        <Text
+          onEnter={() => this.setState({fontSize: 0.2})}
+          onExit={() => this.setState({fontSize: 0.1})}
           style={{
-            color: '#d22',
-            transform: [
-              {translate: [0, 0, -3]},
-              {translateY: 1},
-              {translateX: -0.5},
-              {rotateY: 45},
-              {rotateZ: 45}
-            ]
-          }}
-        />
+            fontSize: this.state.fontSize,
+            transform: [{translate: [0, 0, -2]}]
+        }}>
+          Hover over me!
+        </Text>
       </View>
     )
   }
