675f1e5de76a7a05c15ea9fd0db91bb7f7453f34
Jamison Cozart <jamisoncozart@gmail.com>
2020-04-11 12:31:27 -0700

    Add Cylinder with custom dimensions and position

--- index.vr.js
+++ index.vr.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
 
-import { AppRegistry, asset, Pano, View, Text } from 'react-vr';
+import { AppRegistry, asset, Pano, View, Text, 
+  Cylinder, Box } from 'react-vr';
 
 export default class Basics extends Component {
-  constructor() {
-    super();
-    this.state={
-      fontSize: 0.1
-    }
-  }
-
   render() {
     return (
       <View>
         <Pano source={asset('starry-sky.jpg')}></Pano>
-        <Text
-          onEnter={() => this.setState({fontSize: 0.2})}
-          onExit={() => this.setState({fontSize: 0.1})}
+        <Box
+          dimWidth={0.5}
+          dimHeight={0.5}
+          dimDepth={0.5}
+          wireframe={true}
           style={{
-            fontSize: this.state.fontSize,
-            transform: [{translate: [0, 0, -2]}]
-        }}>
-          Hover over me!
-        </Text>
+            color: '#dd2222',
+            transform: [
+              {translate: [0, 0, -3]},
+              {translateY: 1},
+              {translateX: -0.5},
+              {rotateY: 45},
+              {rotateZ: 45}
+            ]
+          }}
+        />
+        <Cylinder 
+          dimHeight={0.5}
+          radiusBottom={0.5}
+          radiusTop={0.5}
+          segments={15}
+          wireframe={true}
+          style={{
+            color: '#2d2',
+            transform: [
+              {translate: [0, 0.5, -3]},
+              {rotateY: 45},
+              {rotateZ: 45}
+            ]
+          }}
+        />
       </View>
     )
   }
